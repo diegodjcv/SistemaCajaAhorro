4266a57b9bde829ca5168d1f802158261c08f7bd
alexandraguaman19 <44015987+alexandraguaman19@users.noreply.github.com>
2026-07-07 01:06:00 -0500

    alexandraguaman19
    actualización clases DAO

--- src/main/java/ec/edu/ups/app/dao/PersonaDao.java
+++ src/main/java/ec/edu/ups/app/dao/PersonaDao.java
@@ -3,8 +3,6 @@ package ec.edu.ups.app.dao;
 import java.io.Serializable;
 import java.util.List;
 
-import javax.ejb.Stateless;
-import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
@@ -12,7 +10,6 @@ import javax.persistence.TypedQuery;
 
 import ec.edu.ups.app.modelo.Persona;
 
-@ApplicationScoped
 public class PersonaDao implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -20,7 +17,7 @@ public class PersonaDao implements Serializable {
     @Inject
     private EntityManager em;
 
-    public void guardar(Persona persona) {
+    public void insertar(Persona persona) {
         em.persist(persona);
     }
 
@@ -28,57 +25,47 @@ public class PersonaDao implements Serializable {
         em.merge(persona);
     }
 
-        public List<Persona> listar() {
+    public void eliminar(int id) {
+        Persona p = em.find(Persona.class, id);
+        if (p != null) {
+            em.remove(p);
+        }
+        
+    }
 
-        TypedQuery<Persona> query = em.createQuery(
-                "SELECT p FROM Persona p",
-                Persona.class);
+    public Persona buscar(int id) {
+        return em.find(Persona.class, id); 
+    }
 
-        return query.getResultList();
+    public List<Persona> listar() {
+        String jpql = "SELECT p FROM Persona p";
+        return em.createQuery(jpql, Persona.class).getResultList();
     }
 
     public Persona buscarPorCedula(String cedula) {
-
         try {
-
-            TypedQuery<Persona> query = em.createQuery(
-                    "SELECT p FROM Persona p WHERE p.cedula = :cedula",
-                    Persona.class);
-
+            String jpql = "SELECT p FROM Persona p WHERE p.cedula=:cedula";
+            TypedQuery<Persona> query = em.createQuery(jpql, Persona.class);
             query.setParameter("cedula", cedula);
-
             return query.getSingleResult();
-
         } catch (NoResultException e) {
             return null;
         }
     }
 
-    public Persona iniciarSesion(String username, String password) {
-
+    public Persona iniciarSesion(String usuario, String password) {
         try {
-
-            TypedQuery<Persona> query = em.createQuery(
-                    "SELECT p FROM Persona p "
-                    + "WHERE p.username = :username "
-                    + "AND p.password = :password",
-                    Persona.class);
-
-            query.setParameter("username", username);
-            query.setParameter("password", password);
-
+            String jpql = "SELECT p FROM Persona p WHERE p.username=:u AND p.contrasena=:c";
+            TypedQuery<Persona> query = em.createQuery(jpql, Persona.class);
+            query.setParameter("u", usuario);
+            query.setParameter("c", password);
             return query.getSingleResult();
-
         } catch (NoResultException e) {
             return null;
         }
     }
+    
 
-    // Cerrar sesión
     public void cerrarSesion() {
-        System.out.println("Sesión cerrada ");
     }
-
-
-	
 }
