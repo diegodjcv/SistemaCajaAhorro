8982d339fc167862585af43abbe4699f2190ed2b
djcabrera23 <diegodjcv@gmail.com>
2026-07-18 02:39:37 -0500

    diegodjcv
    actualizacion xhtml

--- src/main/java/ec/edu/ups/appdist/Filter/AuthFilter.java
+++ src/main/java/ec/edu/ups/appdist/Filter/AuthFilter.java
@@ -34,15 +34,15 @@ public class AuthFilter implements Filter {
             HttpSession ses = req.getSession(false);
             //  allow user to proccede if url is login2.xhtml or user logged in or user is accessing any page in //public folder
             String reqURI = req.getRequestURI();
-            if ( reqURI.indexOf("/login.xhtml") >= 0 || (ses != null && ses.getAttribute("username") != null)
+            if ( reqURI.indexOf("/login.xhtml") >= 0 || (ses != null && ses.getAttribute("usuario") != null)
                                        || reqURI.indexOf("/public/") >= 0 || reqURI.contains("javax.faces.resource") )
                    chain.doFilter(request, response);
             else   // user didn't log in but asking for a page that is not allowed so take user to login page
-                   res.sendRedirect(req.getContextPath() + "/principal.xhtml");  // Anonymous user. Redirect to login page
+                   res.sendRedirect(req.getContextPath() + "/login.xhtml");  // Anonymous user. Redirect to login page
             
-            if ( reqURI.indexOf("/principal.xhtml") >= 0
-                    || reqURI.indexOf("/public/") >= 0 || reqURI.contains("javax.faces.resource") )
-			chain.doFilter(request, response);
+           // if ( reqURI.indexOf("/login.xhtml") >= 0
+             //       || reqURI.indexOf("/public/") >= 0 || reqURI.contains("javax.faces.resource") )
+			//chain.doFilter(request, response);
 			
          
          
